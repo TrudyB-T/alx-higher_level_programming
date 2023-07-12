b0fca7e92cb1c44b0007c2bc15bfc768a373ce85
TrudyB-T <drtrudybannerman.com>
2023-07-11 22:03:20 -0700

    print tebahlpa

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 d = 0
-for b in range(ord('z'), ord('a') -1, -1):
-    print("{}".format(chr(b - d)),end="")
+for b in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(b - d)), end="")
     d = 32 if d == 0 else 0
