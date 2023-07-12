dcf287eba513c8c210d33910d893d478f107223b
TrudyB-T <drtrudybannerman.com>
2023-07-11 20:02:28 -0700

    print comb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for figure in range(0, 100):
     if figure == 99:
         print(f"{figure}")
     else:
-        print("{:02}".format(figure), end=",")
+        print("{:02}".format(figure), end=", ")
