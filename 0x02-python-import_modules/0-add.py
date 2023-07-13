565f8f3e939a959d8b31fb48bc73826dfcd99a0f
TrudyB-T <drtrudybannerman.com>
2023-07-13 01:54:15 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
+if __name__ == "__main__":
+    import add_0
+    a = 1
+    b = 2
 
-result = add_0.add(a, b)
-print(f"{a} + {b} = {result}")
+    result = add_0.add(a, b)
+    print(f"{a} + {b} = {result}")
