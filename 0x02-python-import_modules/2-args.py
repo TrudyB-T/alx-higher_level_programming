53a97a13043f45600f839c425017246081e71361
TrudyB-T <drtrudybannerman.com>
2023-07-13 19:16:27 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
         print("0 arguments.")
 
     elif length == 1:
-        print("1 argument")
+        print("1 argument:")
 
     else:
         print(f"{length} arguments:")
 
-    for i in range(length):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"{i}: {arg}")
