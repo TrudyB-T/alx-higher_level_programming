0072c608e3b6d3447524570115130bd433100c1b
TrudyB-T <drtrudybannerman.com>
2023-07-17 08:33:10 -0700

    max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
-        return None
+    if my_list is None or len(my_list) == 0:
+        return (None)
     else:
         max_int = my_list[0]
         for i in range(len(my_list)):
