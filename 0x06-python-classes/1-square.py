df1137a9b8691a4b501b3e345f2259a54cb73c74
TrudyB-T <drtrudybannerman.com>
2023-08-01 08:28:27 -0700

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class square"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size):
+        """ passing initial values to object
+
+        Args:
+            size (int): size of the square
+
+        Attributes:
+
+            __size (int): size of the square
+        """
+        self.__size = size
