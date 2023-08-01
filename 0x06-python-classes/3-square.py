7cd9ef7f1a6ff803161f3a4c649bb1541499d588
TrudyB-T <drtrudybannerman.com>
2023-08-01 13:31:04 -0700

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
+"""class square"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size=0):
+        """ passing initial values to object
+
+        Args:
+            size (int): size of the square
+
+        Attributes:
+
+            __size (int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
     def area(self):
         """returns the current square area"""
         result = self.__size * self.__size
