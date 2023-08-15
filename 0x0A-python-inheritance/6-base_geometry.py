019e369ad970eb5d1e41252ccd2a59a5686d15c5
TrudyB-T <drtrudybannerman.com>
2023-08-14 19:18:54 -0700

    base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ class BaseGeometry
+    """
+    def area(self):
+        """raises an Exception with the message area()
+        is not implemented
+        """
+        raise Exception("area() is not implemented")
