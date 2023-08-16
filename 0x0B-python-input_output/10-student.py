8c4474d02bcd1ccc08ac7904187197c33c9400b1
TrudyB-T <drtrudybannerman.com>
2023-08-15 17:46:35 -0700

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiate a new Student
+
+        Arguments:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance
+
+        Arguments:
+        attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {j: getattr(self, j) for j in attrs if hasattr(self, j)}
+        return self.__dict__
