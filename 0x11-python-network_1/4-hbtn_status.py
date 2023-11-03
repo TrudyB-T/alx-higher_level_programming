f745716c031239363f3738b51240adcdedd156a6
TrudyB-T <drtrudybannerman@gmail.com>
2023-11-03 09:11:30 -0700

    hbtn status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" fetches https://alx-intranet.hbtn.io/status"""
+
+import requests
+
+if __name__ == "__main__":
+    rep = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print(f"\t- type: {type(rep.text)}")
+    print(f"\t- content: {rep.text}")
